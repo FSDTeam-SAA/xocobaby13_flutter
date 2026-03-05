3b5c4366ffbe184761c9d80945bd0cb6cfc15ee3
Rabby-551 <rabby.raziul@gmail.com>
2026-03-05 10:05:57 +0600

    chage the app logo

--- lib/feature/auth/onboarding/widgets/onboarding_splash_logo.dart
+++ lib/feature/auth/onboarding/widgets/onboarding_splash_logo.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class OnboardingSplashLogo extends StatelessWidget {
-  const OnboardingSplashLogo({super.key, this.size = 168});
+  const OnboardingSplashLogo({super.key, this.size = 200});
 
   final double size;
 
   @override
   Widget build(BuildContext context) {
     return Image.asset(
-      'assets/onboarding/logo_zocobaby.png',
+      'assets/images/bob_logo.png',
       width: size,
       height: size,
       fit: BoxFit.contain,
